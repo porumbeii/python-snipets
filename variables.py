7fd0bff0e232c771b86d51d84c58bcc6228df35c
porumbeii <66866296+porumbeii@users.noreply.github.com>
2020-10-30 16:37:12 +0200

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,20 @@
+#String variable
+string = "Bla, bla, bla..."
+
+#Number variable
+number = 1.0
+
+#Boolean
+boolean = False
+boolean = True
+
+#List
+list = ["bananas", "cheese", "butter", "bread", "milk"]
+
+#Dictionary
+dictionary = {"bananas" ; 5, "cheese" : "double 100g pack", "butter" ; "60% fat, 50g"}
+
+#Multi-line string
+string = """Story:
+Once apon a time there was a python.
+It created a programing language and it lived happily ever after"""
