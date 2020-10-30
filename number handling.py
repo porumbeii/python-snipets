ad951c1b48702c80af38db53bf91ac1c8c3af617
porumbeii <66866296+porumbeii@users.noreply.github.com>
2020-10-30 16:44:31 +0200

    Create number handling.py

---
+++ number handling.py
@@ -0,0 +1,22 @@
+#First create a variable to use
+x = 10
+y = 20
+
+#Now let's play with them!
+#Tip: if you want to display a variable use print()
+
+#add
+print(x + y)
+#returns 30
+
+#subtract
+print(y - x)
+#returns 10
+
+#divide
+print(y/x)
+#returns 2
+
+#multiply
+print(y*x)
+#returns 200
